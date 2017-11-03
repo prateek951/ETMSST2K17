92cf3d28b7d5eb1743eaf8450f4cb597046ba171
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-11-03 21:07:03 +0530

    Add files via upload

---
+++ src/javaconnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Parteek
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn;
+    PreparedStatement pst;
+    ResultSet rs;
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Parteek\\Documents\\NetBeansProjects\\EMS-ST2K17\\ems.sqlite");
+            return conn;
+        }catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+    }
+   }
+}
